71dc8440b938932131d388571510c78ad89bd831
Jaipavan S <jaipavansaravanan2009@gmail.com>
2026-03-02 00:27:12 +0530

    added create exam route

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ app.get("/", (req, res) => {
   res.send("Exam System API Running 🚀");
 });
 
+// Temporary storage
+let exams = [];
+
+// CREATE EXAM
+app.post("/api/exams", (req, res) => {
+  const { title, questions } = req.body;
+
+  if (!title || !questions) {
+    return res.status(400).json({ message: "Title and questions required" });
+  }
+
+  const newExam = {
+    id: Date.now(),
+    title,
+    questions
+  };
+
+  exams.push(newExam);
+
+  res.status(201).json({
+    message: "Exam created successfully ✅",
+    exam: newExam
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
